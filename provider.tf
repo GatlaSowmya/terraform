3a88ca369bfa93e2185e55e069ba2c93189698b9
GatlaSowmya <gatlasowmya8@gmail.com>
2023-10-04 11:46:09 +0530

    i was creaed terraform file

---
+++ provider.tf
@@ -0,0 +1,11 @@
+provider "azurerm" {
+  features {}
+
+
+  subscription_id    = "fadaacfa-db50-4251-b3b9-c1c5838955a2"
+  client_id       = "7775287e-0225-4d67-bfe6-2b8f92418e7b"
+  client_secret  = "w4d8Q~8JXeypykBDOhYSkYnGHoyapFOsHHdFSa9R"
+  tenant_id       = "d2880c00-328c-4e70-9a96-c2298a2745b0"
+
+}
+
